2a552253bb42c71da709a5d0b45ef97549cab873
sedoi89 <rkyar76@gmail.com>
2022-06-18 23:50:51 +0300

    First Load

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Card from "./components/card/card";
 
 function App() {
     const cards = [{
-        id: 1, with: 'фуа-гра', portion: 10, present: '', kg: '0,5'
+        id: 1, with: 'фуа-гра', portion: 10, present: '', kg: '0,5', tagline: 'Печень утки разварная с артишоками.', disabled:false
     }, {
-        id: 2, with: 'рыбой', portion: 40, present: '2', kg: '2'
-    }, { id: 3, with: 'курой', portion: 100, present: '5', kg: '5'
+        id: 2, with: 'рыбой', portion: 40, present: '2', kg: '2', tagline: 'Головы щучьи с чесноком да свежайшая сёмгушка.', disabled:false
+    }, { id: 3, with: 'курой', portion: 100, present: '5', kg: '5', tagline: 'Филе из цыплят с трюфелями в бульоне.', disabled:true
         }]
     return (
-        <div className={'main'}> <p className={'main-title'}> Ты сегодня покормил кота? </p>
+      <div className={'main-container'} style={main}>
+          <p className={'main-title'}> Ты сегодня покормил кота? </p>
+        <div className={'main'}>
             {cards.map(card =>
                 <Card
                     props={card}
@@ -18,6 +20,7 @@ function App() {
                 />
             )}
         </div>
+      </div>
     );
 }
 
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CardModule from "../card_module";
+
 
 const Card = (props) => {
-    console.log(props)
+
+    const [leave, setLeave] = useState('');
+    const [enter, setEnter] = useState('');
+    const toggle = function () {
+        if (leave === '') {
+            setLeave('leave')
+            setEnter('entered')
+        } else {
+            setLeave('')
+            setEnter('')
+        }
+    }
+    const disabled = function (props) {
+        if (props.disabled) {
+            return <p className={'disabled-text'}>Пичалька, c {props.with} закончился.</p>
+        } else if (leave === 'leave') {
+            return <p className={'link-rerendered'}>{props.tagline}</p>
+        } else {
+            return <p className="link">Что сидишь? Порадуй котэ, <span
+                onClick={toggle}>купи.</span></p>
+        }
+    }
     return (
-      <div className={'container'}>
-        <div className="Card">
-            <div className={'Corner'}></div>
-            <p className={'title'}>Сказочное заморское яство</p>
-            <h1>Нямушка</h1>
-            <h2>c {props.props.with}</h2>
-            <p className={'present'}>{props.props.portion} порций <br/> {props.props.present} {props.props.portion === 10? <span>мышь</span> : <span>мыш{props.props.present <5? <span>и</span> : <span>ей</span>}</span>} в подарок</p>
-            <img className={'cat'} src={'/img/cat.png'}/>
-            <div className={'round'}><span className={'text'}>{props.props.kg}</span> <span className={'text_2'}>кг</span> </div>
+
+        <div className={`container`}>
+            <div className={`Card `} onClick={toggle}>
+                <CardModule
+                    leave={leave}
+                    entered={enter}
+                    disabled={props.props.disabled}
+                />
+                <div className={`${props.props.disabled ? 'disabled' : 'notDisabled'}`}>
+                    <p className={'title'}>Сказочное заморское яство</p>
+                    <h1>Нямушка</h1>
+                    <h2>c {props.props.with}</h2>
+                    <p className={'present'}>{props.props.portion} порций <br/> {props.props.present} {props.props.portion === 10 ?
+                        <span>мышь</span> :
+                        <span>мыш{props.props.present < 5 ? <span>и</span> : <span>ей</span>}</span>} в подарок</p>
+                    <img className={'cat'} src={'/img/cat.png'} alt={'Котик'}/>
+                    <div className={`round ${leave}-back ${props.props.disabled ? 'disabled-background' : ''}`}><span
+                        className={'text'}>{props.props.kg}</span> <span
+                        className={'text_2'}>кг</span></div>
+                </div>
+            </div>
+            {disabled(props.props)}
         </div>
-          <p className="link">Что сидишь? Порадуй котэ, <a>купи.</a></p>
-      </div>
+
     );
 };
 
